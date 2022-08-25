de8b248388a3dc8173dc69f75f9bb236024da905
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-26 00:06:59 +0300

    Add files via upload

---
+++ EXPLDRER1/index.js
@@ -0,0 +1,7 @@
+//ReactDOM.render(<h3>But, if you're thinkin' about my baby
+//It don't matter if you're black or white!</h3>, document.getElementById("root"))
+
+ReactDOM.render(
+  <ul><li>thing i</li><li>thing 2</li></ul>, 
+  document.getElementById("root")
+)
