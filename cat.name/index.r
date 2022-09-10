f4f26d9287b4fa83854bb087db827db8bfac3776
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-09-11 00:33:20 +0300

    Add files via upload

---
+++ cat.name/index.r
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page = (
+  <div>
+      <h1>My awesome website in React</h1>
+      <h3>names for cot</h3>
+      <ol>
+          <li>babel</li>
+          <li>popy</li>
+          <li>gorga</li>
+          <li>chity chity baan baan</li>
+      </ol>
+  </div>
+)
+
+
+document.getElementById("root").append(JSON.stringify(page))
